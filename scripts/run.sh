5f56e760c5e5353616ca7d9a129b0ce52a1eb8d4
kasper <kasper.engelen@hotmail.com>
2025-06-19 22:44:32 +0200

    Typo.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,6 @@
 SEED=$1
 ITER=$2
 
-echo "Running iterion '${ITER}' with seed '${SEED}'"
+echo "Running iteration '${ITER}' with seed '${SEED}'"
 
 python truck_trailer_multi_stage_loop_operating_script.py "${SEED}" "${ITER}"
